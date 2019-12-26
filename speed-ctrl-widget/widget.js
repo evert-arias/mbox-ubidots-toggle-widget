7694e25e7fef61dec952c31e87e2564149509bf8
Evert Arias <evert@codefor.me>
2019-12-26 17:34:37 -0500

    Update

--- speed-ctrl-widget/widget.js
+++ speed-ctrl-widget/widget.js
@@ -7,6 +7,7 @@ var IN_VARIABLE_ID = '';
 // OUTPUT VARIABLE
 var OUT_VARIABLE_ID = '';
 
+
 // HTML elements
 var button25hz = $('#mbox-btn-25hz');
 var button35hz = $('#mbox-btn-35hz');
@@ -16,191 +17,182 @@ var label = $('#mbox-widget-label');
 var spinner = $('#mbox-widget-spinner');
 var buttonGroup = $('#mbox-button-group');
 
-// Global variables
-var isWaiting = false;
-var motorSpeedRef;      // Motor Speed Reference
-var update_ms = 1000; 	// Update interval in ms
-var timeout;            // Timeout object
-var timeout_ms = 60000; // Timeout value in ms
+const NO_RESPONSE = 0; 	// Status: No reponse
+const DONE = 1; 		// Status: Done
+const SENDING = 2; 		// Status: Sending
+const WAITING = 3; 		// Status: Waiting
+
+var status;							// Status
+var received = {};      			// Motor Speed Reference
+var update_ms = 1000; 				// Update interval in ms
+var response_timeout;      			// Timeout object
+var response_timeout_ms = 30000; 	// Timeout value in ms
 
 // Get data from variable
 function getDataFromVariable(variable, token, callback) {
-    var url = 'https://industrial.api.ubidots.com/api/v1.6/variables/' + variable + '/values';
-    var headers = {
-        'X-Auth-Token': token,
-        'Content-Type': 'application/json'
-    };
+	var url = 'https://industrial.api.ubidots.com/api/v1.6/variables/' + variable + '/values';
+	var headers = {
+		'X-Auth-Token': token,
+		'Content-Type': 'application/json'
+	};
 
-    $.ajax({
-        url: url,
-        method: 'GET',
-        headers: headers,
-        success: function (res) {
-            callback(res.results);
-        }
-    });
+	$.ajax({
+		url: url,
+		method: 'GET',
+		headers: headers,
+		success: function (res) {
+			callback(res.results);
+		}
+	});
+}
+
+// Set response timeout
+function setResponseTimeout() {
+	if (response_timeout) {
+		clearTimeout(response_timeout);
+		response_timeout = null;
+	}
+	response_timeout = setTimeout(() => {
+		updateUI(NO_RESPONSE);
+	}, response_timeout_ms);
 }
 
 // Send value
 function sendValue(variable, valueToSend, token, callback) {
-    var url = 'https://industrial.api.ubidots.com/api/v1.6/variables/' + variable + '/values';
-    var headers = {
-        'Content-Type': 'application/json',
-        'X-Auth-Token': TOKEN
-    };
+	var url = 'https://industrial.api.ubidots.com/api/v1.6/variables/' + variable + '/values';
+	var headers = {
+		'Content-Type': 'application/json',
+		'X-Auth-Token': TOKEN
+	};
 
-    $.ajax({
-        data: JSON.stringify({
-            value: valueToSend
-        }),
-        method: 'POST',
-        url: url,
-        headers: headers,
-        success: function (res) {
-            callback(res.value);
-        }
-    });
+	$.ajax({
+		data: JSON.stringify({
+			value: valueToSend
+		}),
+		method: 'POST',
+		url: url,
+		headers: headers,
+		success: function (res) {
+			// Start response timeout
+			setResponseTimeout();
+			// Callback
+			callback(res.value);
+		}
+	});
 }
 
-// Button 25Hz Click
+// Handle button25Hz click
 button25hz.on('click', function () {
-
-    // Update status label
-    label.text("Sending...");
-    label.removeClass();
-    label.addClass("text-primary text-wrap text-monospace");
-
-    // Send
-    sendValue(OUT_VARIABLE_ID, 2500, TOKEN, function (value) {
-        // Set status = waiting
-        isWaiting = true;
-        updateUI(true);
-        // Set timeout
-        timeout = setTimeout(() => {
-            updateUI(false);
-            label.text("Timeout: No response from device");
-            label.removeClass();
-            label.addClass("text-danger text-wrap text-monospace");
-        }, timeout_ms);
-    });
+	// Set status = sending
+	updateUI(SENDING);
+	// Send
+	sendValue(OUT_VARIABLE_ID, 2500, TOKEN, function (value) {
+		// Set status = waiting
+		updateUI(WAITING);
+	});
 });
 
-// Button 35Hz Click
+// Handle button35Hz click
 button35hz.on('click', function () {
-
-    // Update status label
-    label.text("Sending...");
-    label.removeClass();
-    label.addClass("text-primary text-wrap text-monospace");
-
-    // Send
-    sendValue(OUT_VARIABLE_ID, 3500, TOKEN, function (value) {
-        // Set status = waiting
-        isWaiting = true;
-        updateUI(true);
-        // Set timeout
-        timeout = setTimeout(() => {
-            updateUI(false);
-            label.text("Timeout: No response from device");
-            label.removeClass();
-            label.addClass("text-danger text-wrap text-monospace");
-        }, timeout_ms);
-    });
+	// Set status = sending
+	updateUI(SENDING);
+	// Send
+	sendValue(OUT_VARIABLE_ID, 3500, TOKEN, function (value) {
+		// Set status = waiting
+		updateUI(WAITING);
+	});
 });
 
-// Button 45Hz Click
+// Handle button45Hz click
 button45hz.on('click', function () {
-
-    // Update status label
-    label.text("Sending...");
-    label.removeClass();
-    label.addClass("text-primary text-wrap text-monospace");
-
-    // Send
-    sendValue(OUT_VARIABLE_ID, 4500, TOKEN, function (value) {
-        // Set status = waiting
-        isWaiting = true;
-        updateUI(true);
-        // Set timeout
-        timeout = setTimeout(() => {
-            updateUI(false);
-            label.text("Timeout: No response from device");
-            label.removeClass();
-            label.addClass("text-danger text-wrap text-monospace");
-        }, timeout_ms);
-    });
+	// Set status = sending
+	updateUI(SENDING);
+	// Send
+	sendValue(OUT_VARIABLE_ID, 4500, TOKEN, function (value) {
+		// Set status = waiting
+		updateUI(WAITING);
+	});
 });
 
-// Button 60Hz Click
+// Handle button60Hz click
 button60hz.on('click', function () {
-
-    // Update status label
-    label.text("Sending...");
-    label.removeClass();
-    label.addClass("text-primary text-wrap text-monospace");
-
-    // Send
-    sendValue(OUT_VARIABLE_ID, 6000, TOKEN, function (value) {
-        // Set status = waiting
-        isWaiting = true;
-        updateUI(true);
-        // Set timeout
-        timeout = setTimeout(() => {
-            updateUI(false);
-            label.text("Timeout: No response from device");
-            label.removeClass();
-            label.addClass("text-danger text-wrap text-monospace");
-        }, timeout_ms);
-    });
+	// Set status = sending
+	updateUI(SENDING);
+	// Send
+	sendValue(OUT_VARIABLE_ID, 6000, TOKEN, function (value) {
+		// Set status = waiting
+		updateUI(WAITING);
+	});
 });
 
 // Update widget UI based on status
-function updateUI(waiting) {
-    if (waiting) {
-        // Button Group
-        buttonGroup.hide();
-        // Spinner
-        spinner.show();
-        // Status label
-        label.text("Waiting for response");
-        label.removeClass();
-        label.addClass("text-secondary text-wrap text-monospace");
-    } else {
-        // Button Group
-        buttonGroup.show();
-        // Spinner
-        spinner.hide();
-        // Status label
-        label.text("Selected frequency: " + motorSpeedRef + "Hz");
-        label.removeClass();
-        label.addClass("text-success text-wrap text-monospace");
-    }
+function updateUI(new_status) {
+	status = new_status;
+
+	if (status === NO_RESPONSE) {
+		// Button Group
+		buttonGroup.show();
+		// Spinner
+		spinner.hide();
+		// Label
+		label.text("Timeout: No response from device");
+		label.removeClass();
+		label.addClass("text-danger text-wrap text-monospace");
+	} else if (status === DONE) {
+		// Button Group
+		buttonGroup.show();
+		// Spinner
+		spinner.hide();
+		// Label
+		label.text("Selected frequency: " + received.value + "Hz");
+		label.removeClass();
+		label.addClass("text-success text-wrap text-monospace");
+	} else if (status === SENDING) {
+		// Button Group
+		buttonGroup.hide();
+		// Spinner
+		spinner.show();
+		// Label
+		label.text("Sending...");
+		label.removeClass();
+		label.addClass("text-primary text-wrap text-monospace");
+	} else if (status === WAITING) {
+		// Button Group
+		buttonGroup.hide();
+		// Spinner
+		spinner.show();
+		// Label
+		label.text("Waiting for response");
+		label.removeClass();
+		label.addClass("text-secondary text-wrap text-monospace");
+	}
 }
 
 // Periodically read data from variable
 function update() {
-    var interval = 1000;
+	var interval = 1000;
 
-    // Get data from variable every 1000 ms
-    setInterval(() => {
-        getDataFromVariable(IN_VARIABLE_ID, TOKEN, function (values) {
-            var value = values[0].value;
+	// Get data from variable every 1000 ms
+	setInterval(() => {
+		getDataFromVariable(IN_VARIABLE_ID, TOKEN, function (values) {
+			var lastValue = values[0];
 
-            // Ignore if same state 
-            if (motorSpeedRef === value) {
-                return;
-            }
-            
-            // Reset timeout
-            clearTimeout(timeout);
+			// Ignore if same state 
+			if (received.timestamp === lastValue.timestamp) {
+				return;
+			}
 
-            // Copy new speed ref
-            motorSpeedRef = value;
+			// Reset timeout
+			clearTimeout(response_timeout);
+			response_timeout = null;
 
-            isWaiting = false;
-            updateUI(false);
-        });
-    }, update_ms);
+			// Copy new speed ref
+			received = lastValue;
+
+			updateUI(DONE);
+
+		});
+	}, update_ms);
 }
 
 // Execute update
